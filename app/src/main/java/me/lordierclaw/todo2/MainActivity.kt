21de6dcda4af0b4f1113d7c2dab9cfd1c95495e5
LordierClaw <lordierclaw@gmail.com>
2023-08-29 16:38:04 +0700

    Setup bottom nav for preview

--- app/src/main/java/me/lordierclaw/todo2/MainActivity.kt
+++ app/src/main/java/me/lordierclaw/todo2/MainActivity.kt
@@ -2,17 +2,22 @@ package me.lordierclaw.todo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import me.lordierclaw.todo2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNav: BottomNavigationView
+    private lateinit var navHost: NavHostFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.bottomNav.setupWithNavController(findNavController(R.id.nav_host))
+        bottomNav = binding.bottomNav
+        navHost = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        bottomNav.setupWithNavController(navHost.navController)
     }
 }
